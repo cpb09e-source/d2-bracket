4b1c79e194f1fca8297558686b7361a4ba11a225
Colin <you@example.comcpb09e@gmail.com>
2026-03-11 15:06:22 -0500

    Enable Firestore long polling fallback again

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = initializeFirestore(app, {
-  experimentalAutoDetectLongPolling: true,
+  experimentalForceLongPolling: true,
   useFetchStreams: false,
 });
